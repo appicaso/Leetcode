9878ce2adb293d5140e5d49905cf230a7a96b143
Serge Sroujian <ssroujian@outlook.com>
2023-12-26 20:53:43 -0500

    Time: 930 ms (64.24%), Space: 0B (100.00%) - LeetHub

--- 1075-project-employees-i/1075-project-employees-i.sql
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -1,18 +1,9 @@
 SELECT
-    project_id,
-    ROUND(AVG(experience_years),2) as average_years
-
-FROM 
-    (
-        SELECT
-            p.project_id,
-            p.employee_id,
-            e.experience_years
-        FROM
-            Project p
-        LEFT JOIN Employee e 
-             ON p.employee_id = e.employee_id
-    ) AS all_projects
-
-GROUP BY p.project_id
-
+    p.project_id,
+    ROUND(AVG(e.experience_years), 2) AS average_years
+FROM
+    Project p
+JOIN
+    Employee e ON p.employee_id = e.employee_id
+GROUP BY
+    p.project_id
